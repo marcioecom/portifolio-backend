cbb66d74e6a62bda6f2ac3cba8b6cf80d1a673fb
Marcio Junior <marcio.mj70@gmail.com>
2022-07-29 12:27:37 -0300

    chore(url): change shortUrl to opcional

--- src/modules/urls/domain/Url.ts
+++ src/modules/urls/domain/Url.ts
@@ -2,10 +2,8 @@ import { z } from 'zod';
 
 export const urlSchema = z.object({
   redirectUrl: z.string()
-    .url({ message: 'Url inválida' }),
-  shortUrl: z.string()
-    .min(3, { message: 'A URL curta precisa ter no mínimo 3 caracteres' })
-    .optional()
+    .url({ message: 'Formato da url inválida' }),
+  shortUrl: z.string().optional()
 });
 
 export type IUrl = z.infer<typeof urlSchema>
